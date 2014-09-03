3f73cd00a7a6f6c5974db3a064eb70ac8c83e131
jjshammas <john@Johns-MacBook-Pro.local>
2014-09-03 15:40:32 -0400

    fixing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ var concat = require("gulp-concat");
 var prettify = require("gulp-js-prettify");
 
 gulp.task("concat", function() {
-	return
-	gulp.src("tests/**/*.js")
+	return gulp.src("tests/**/*.js")
 		.pipe(concat("all.js"))
 		.pipe(prettify())
 		.pipe(gulp.dest("./"));
