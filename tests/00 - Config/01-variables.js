941e29f9686493a146af297f8f2e578ac00f3e67
John Shammas <john@johnshammas.com>
2014-09-03 15:51:07 -0400

    Updating Staging URL

--- tests/00 - Config/01-variables.js
+++ tests/00 - Config/01-variables.js
@@ -1,5 +1,5 @@
 // The environment to use
-var env = "http://heavymeddle.dynalias.net:9080";
+var env = "http://heavymeddle.dynalias.net:82";
 
 // The standard wait time to use when waiting for AJAX requests to finish
 var wait = 2000;
